f6c1b63c793c15b80ddbee7d25a76f5c280c2cf1
Marq Roldan <hcmlroldan@gmail.com>
2021-03-15 23:06:55 +0800

    #11 Fixed linting issues

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
             @import "@/scaffolding/sass/_variables.scss";
             @import "@/scaffolding/sass/_functions.scss";
             @import "@/scaffolding/sass/_mixins.scss";
-          `,
-      },
-    },
-  },
+          `
+      }
+    }
+  }
 };
