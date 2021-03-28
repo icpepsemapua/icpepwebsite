060744ec892be054979c44338ce376f988d6aac5
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2021-03-28 23:53:10 +0800

    Added NewsCard Component

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>{{ imageUrl }}</div>
+    <div>{{ headline }}</div>
+    <div>{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewsCard",
+  props: {
+    imageUrl: String,
+    headline: String,
+    content: String
+  }
+};
+</script>
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="news">
-    <h1>This is the news page</h1>
+    <h1>NEWS</h1>
+    <NewsCard
+      imageUrl="https://picsum.photos/200/300"
+      headline="Headline"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus molestie dapibus. Nulla quis pellentesque nulla, posuere vulputate orci. Nam fringilla turpis sapien, eget pretium lorem auctor ac. Mauris et accumsan justo. Curabitur eget turpis purus. Fusce cursus id justo sit amet bibendum. Quisque iaculis felis nec ullamcorper hendrerit."
+    />
   </div>
 </template>
+
+<script>
+import NewsCard from "../components/NewsCard";
+export default {
+  name: "News",
+  components: { NewsCard }
+};
+</script>
