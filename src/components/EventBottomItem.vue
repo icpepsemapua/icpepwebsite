59feb2a4ea5bc2354c51d08cceeb6d1aabf9b156
Karl Taguibao <karltag2018@gmail.com>
2021-09-03 18:59:39 +0800

    Updated Events page and added event component.

---
+++ src/components/EventBottomItem.vue
@@ -0,0 +1,49 @@
+<template lang="">
+  <div class="eventBottom">
+    <img class="eventBottom__item eventBottom__item--image" :src="img" />
+    <div class="eventBottom__item eventBottom__item--title">{{ title }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "EventItemBottom",
+  props: {
+    img: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.eventBottom {
+  background: #24315e;
+  padding: 40px;
+  &__item {
+    font-size: 14px;
+    font-family: "Roboto", sans-serif;
+    text-align: left;
+    &--image {
+      max-width: 696px;
+      @media only screen and (max-width: 1000px) {
+        max-width: 80%;
+      }
+      @media only screen and (max-width: 700px) {
+        max-width: 95%;
+      }
+      object-fit: fill;
+      border-radius: 35px;
+    }
+    &--title {
+      font-family: "Ubuntu", sans-serif;
+      font-size: 24px;
+      font-weight: bold;
+      color: #f7e9a0;
+      text-align: center;
+    }
+  }
+}
+</style>
--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,5 +1,108 @@
 <template>
   <div class="events">
-    <h1>This is the Events page</h1>
+    <div class="events__container">
+      <EventItem
+        v-for="eventArticle in eventArticles"
+        :key="eventArticle.id"
+        :img="eventArticle.img"
+        :title="eventArticle.title"
+        :type="eventArticle.type"
+        :desc="eventArticle.desc"
+        :date="eventArticle.date"
+        :endDate="eventArticle.endDate"
+        :showEventButton="eventArticle.showEventButton"
+      />
+    </div>
+    <div class="events__carousel">
+      <carousel
+        :items-to-show="carouselNumber"
+        class="carousel-inner-container"
+      >
+        <slide v-for="slide in eventsBottom" :key="slide">
+          <EventItemBottom
+            :key="slide.id"
+            :img="slide.img"
+            :title="slide.title"
+          />
+        </slide>
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
+    </div>
   </div>
 </template>
+
+<script>
+import EventItem from "../components/EventItem.vue";
+import EventItemBottom from "../components/EventBottomItem.vue";
+import { eventArticles } from "../assets/events";
+import { eventsBottom } from "../assets/eventsBottom";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+export default {
+  data() {
+    return {
+      carouselNumber: 2,
+    };
+  },
+  components: {
+    EventItem,
+    EventItemBottom,
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  methods: {
+    windowSizeChange() {
+      if (window.innerWidth <= 1450) {
+        this.carouselNumber = 1;
+      } else {
+        this.carouselNumber = 2;
+      }
+    },
+  },
+  mounted() {
+    this.windowSizeChange();
+    window.addEventListener("resize", this.windowSizeChange);
+  },
+  setup() {
+    return {
+      eventArticles,
+      eventsBottom,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  background: #bcc9f8;
+}
+.events {
+  .events__container {
+    display: inline-grid;
+    grid-template-columns: auto auto auto;
+    grid-gap: 30px 50px;
+    @media only screen and (max-width: 1000px) {
+      grid-template-columns: auto auto;
+    }
+    @media only screen and (max-width: 870px) {
+      grid-template-columns: auto;
+    }
+    padding-top: 30px;
+    padding-bottom: 40px;
+  }
+  .events__carousel {
+    width: 100%;
+    background: #25315a;
+    .carousel-inner-container {
+      margin-right: 30px;
+      margin-left: 30px;
+      padding-bottom: 30px;
+    }
+  }
+}
+</style>
