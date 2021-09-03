87d53c75204330b2ae1d8507cb77eaaa38a6d026
Karl Taguibao <karltag2018@gmail.com>
2021-09-03 19:00:03 +0800

    Added placeholder main event data.

---
+++ src/assets/events.js
@@ -0,0 +1,47 @@
+export const eventArticles = [
+  {
+    id: 1,
+    img: "./Screenshot_1.png",
+    title: "Google Cloud for Students",
+    type: "Webinar",
+    desc:
+      "Digital learning requires not only accessing internet sources, but also storing data online. Discover more about Google Cloud Services and its practicel uses on your student life with Mr. Mark Earvin Sarmiento.",
+    date: new Date(2020, 11, 6, 14, 30),
+    endDate: new Date(2020, 11, 6, 16, 0),
+    showEventButton: true,
+  },
+  {
+    id: 2,
+    img: "./Screenshot_1.png",
+    title: "Google Cloud for Students",
+    type: "Webinar",
+    desc:
+      "Digital learning requires not only accessing internet sources, but also storing data online. Discover more about Google Cloud Services and its practicel uses on your student life with Mr. Mark Earvin Sarmiento.",
+    date: new Date(2020, 11, 6, 14, 30),
+    endDate: new Date(2020, 11, 6, 16, 0),
+    showEventButton: true,
+  },
+  {
+    id: 3,
+    img: "./Screenshot_1.png",
+    title: "Google Cloud for Students",
+    type: "Webinar",
+    desc:
+      "Digital learning requires not only accessing internet sources, but also storing data online. Discover more about Google Cloud Services and its practicel uses on your student life with Mr. Mark Earvin Sarmiento.",
+    date: new Date(2020, 11, 6, 14, 30),
+    endDate: new Date(2020, 11, 6, 16, 0),
+    // eslint-disable-next-line prettier/prettier
+    showEventButton: true,
+  },
+  {
+    id: 4,
+    img: "./Screenshot_1.png",
+    title: "Google Cloud for Students",
+    type: "Webinar",
+    desc:
+      "Digital learning requires not only accessing internet sources, but also storing data online. Discover more about Google Cloud Services and its practicel uses on your student life with Mr. Mark Earvin Sarmiento.",
+    date: new Date(2020, 11, 6, 14, 30),
+    endDate: new Date(2020, 11, 6, 16, 0),
+    showEventButton: true,
+  },
+];
