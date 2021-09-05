2be9d9f4bb3b329a3e08e459986a2b9d2482d493
Foncemere <61183588+Foncemere@users.noreply.github.com>
2021-09-06 01:24:57 +0800

    Dev #7 - Events screen (#28)
    * placeholder data for event page bottom portion
    
    * Added vue3-carousel dependency.
    
    * Package lock json update.
    
    * Updated Events page and added event component.
    
    * Added placeholder main event data.
    
    * placeholder image for event

---
+++ src/assets/eventsBottom.js
@@ -0,0 +1,27 @@
+export const eventsBottom = [
+  {
+    id: 1,
+    img: "./2018NCRChallenge.png",
+    title: "ICPEP.SE-NCR Challenge 2018",
+  },
+  {
+    id: 2,
+    img: "./2018NCRChallenge.png",
+    title: "Christmas Party/ Induction 2Q1819",
+  },
+  {
+    id: 3,
+    img: "./2018NCRChallenge.png",
+    title: "ICPEP.SE-NCR Challenge 2018",
+  },
+  {
+    id: 4,
+    img: "./2018NCRChallenge.png",
+    title: "Christmas Party/ Induction 2Q1819",
+  },
+  {
+    id: 5,
+    img: "./2018NCRChallenge.png",
+    title: "ICPEP.SE-NCR Challenge 2018",
+  },
+];
